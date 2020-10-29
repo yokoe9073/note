0f05b6bcdc546990dc98b88f8424c2d90df25115
yokoe9073 <yokoe9073@gmail.com>
2020-10-30 06:49:52 +0900

    tweetコントローラーにdestroyアクションを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,12 @@ class TweetsController < ApplicationController
   def create
     Tweet.create(tweet_params)
   end
-
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+  
   private
   def tweet_params
     params.require(:tweet).permit(:text)
