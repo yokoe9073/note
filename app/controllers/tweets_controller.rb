482cbe89512ad435a13196fb260d34fe20da5eb6
yokoe9073 <yokoe9073@gmail.com>
2020-10-31 08:24:28 +0900

    レコードの取得順を変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index]
   
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
  
   def new
